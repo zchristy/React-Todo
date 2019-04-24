aa94d4bedd1b42ed10cc7ed51bcbf21599746f18
zchristy <zchristy44@gmail.com>
2019-04-24 08:28:03 -0500

    Added Comments

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    // Seed data
     this.state = {
       todos: [
               {
@@ -30,6 +31,7 @@ class App extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
+    // Create new Object in state array with the user input value
     this.setState({
       todos: [...this.state.todos, {
         id: Date.now(),
@@ -37,34 +39,45 @@ class App extends Component {
         isCompleted: 'false'
       }]
     });
+    // reset input box
     event.target.reset();
   }
 
   handleDelete = (event) => {
+    // slice state todos array to create new array
     const todosArray = [...this.state.todos];
+    // Grab clicked element
     const clickedTask = event.target.getAttribute("delete");
+    // find object index of clicked element and return index
     const index = todosArray.findIndex(todo => {
       return todo.task === clickedTask;
     });
     if(index !== -1) {
+      // remove object from new array
       todosArray.splice(index, 1);
+      // set state to new array
       this.setState({ todos: todosArray});
     }
   }
 
   handleDone = (event) => {
+    // toggle done class when task is clicked
     event.target.classList.toggle('done');
-
+    // slice state todos array to create new array
     const todosArray = [...this.state.todos];
+    // Grab clicked element
     const clickedTask = event.target.getAttribute("complete");
+    // find object index of clicked element and return index
     const index = todosArray.findIndex(todo => {
       return todo.task === clickedTask;
     });
+    // change value of isCompleted in new array
     todosArray[index].isCompleted = true;
+    // set state to new array
     this.setState({ todos: todosArray});
-    console.log(this.state.todos);
   }
 
+  // Combining all click events into one method
   click = (event) => {
     if(event.target.getAttribute('name') === 'task'){
       this.handleDone(event);
